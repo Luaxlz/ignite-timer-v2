11b3a05cd71b08feed0708f0e5cd6c292605d152
Lucas Angeli <jl.angeli@outlook.com>
2024-01-08 14:31:25 -0300

    feat: created header styles and links

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,19 @@
 import { HeaderContainer } from './styles';
+import LogoIgnite from '../../assets/Logo-ignite.svg';
+import { Scroll, Timer } from '@phosphor-icons/react';
+import { NavLink } from 'react-router-dom';
 
 export function Header() {
   return (
     <HeaderContainer>
-      <span>Logo</span>
+      <img src={LogoIgnite} alt='' />
       <nav>
-        <a href=''>timer</a>
-        <a href=''>history</a>
+        <NavLink to='/' title='Timer'>
+          <Timer size={24} />
+        </NavLink>
+        <NavLink to='/history' title='Histórico'>
+          <Scroll size={24} />
+        </NavLink>
       </nav>
     </HeaderContainer>
   );
