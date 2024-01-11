17c2c4ebdac3a424d351beb5d4a241262946b892
Lucas Angeli <jl.angeli@outlook.com>
2024-01-11 16:17:47 -0300

    refactor: reworked the NewCycleForm component to work with the new CyclesContext

--- src/pages/Home/components/NewCycleForm/index.tsx
+++ src/pages/Home/components/NewCycleForm/index.tsx
@@ -1,26 +1,11 @@
-import { zodResolver } from '@hookform/resolvers/zod';
-import { useForm } from 'react-hook-form';
 import { FormContainer, TaskInput, MinutesAmountInput } from './styles';
-import * as zod from 'zod';
-
-const newCycleFormValidationSchema = zod.object({
-  task: zod.string().min(1, 'Informe a tarefa'),
-  minutesAmount: zod
-    .number()
-    .min(5, 'O clico precisa ser de no mínimo 5 minutos')
-    .max(60, 'O clico precisa ser de no máximo 60 minutos'),
-});
-
-type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>;
+import { useContext } from 'react';
+import { CyclesContext } from '../../Index';
+import { useFormContext } from 'react-hook-form';
 
 export function NewCycleForm() {
-  const { register, handleSubmit, watch, reset } = useForm<NewCycleFormData>({
-    resolver: zodResolver(newCycleFormValidationSchema),
-    defaultValues: {
-      task: '',
-      minutesAmount: 0,
-    },
-  });
+  const { activeCycle } = useContext(CyclesContext);
+  const { register } = useFormContext();
 
   return (
     <FormContainer>
