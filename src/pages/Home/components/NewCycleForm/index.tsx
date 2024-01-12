5cad73fc4bd8e2c47a19e14ce701f02883011fbf
Lucas Angeli <jl.angeli@outlook.com>
2024-01-12 09:55:43 -0300

    refactor: reworked the cyclesContexto to provide context to the whole app

--- src/pages/Home/components/NewCycleForm/index.tsx
+++ src/pages/Home/components/NewCycleForm/index.tsx
@@ -1,7 +1,7 @@
 import { FormContainer, TaskInput, MinutesAmountInput } from './styles';
 import { useContext } from 'react';
-import { CyclesContext } from '../../Index';
 import { useFormContext } from 'react-hook-form';
+import { CyclesContext } from '../../../../contexts/CyclesContext';
 
 export function NewCycleForm() {
   const { activeCycle } = useContext(CyclesContext);
